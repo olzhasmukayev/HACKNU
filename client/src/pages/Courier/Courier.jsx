5b5286678a7dfd76085c76510507d27d60100286
Olzhas Mukayev <65302894+olzhasmukayev@users.noreply.github.com>
2023-04-16 00:04:13 +0600

    Add courier animation

--- client/src/pages/Courier/Courier.jsx
+++ client/src/pages/Courier/Courier.jsx
@@ -1,82 +1,149 @@
-import React from "react";
+import Running from "../../assets/running.gif";
+import { React, useEffect, useState } from "react";
 import { GoogleMap, LoadScript, Polyline } from "@react-google-maps/api";
 import { decode, encode } from "@googlemaps/polyline-codec";
 import styles from "./Courier.module.css";
 import useWindowSize from "../../hooks/useWindowSize";
+import axios from "axios";
 
-const center = {
-  lat: 0,
-  lng: -180,
+const computeRoutes = async (origin, destination) => {
+  const API_KEY = "AIzaSyD-iUnnCzlz-eGSWYRJnfEvSA_lhg24CqU";
+
+  try {
+    const headers = {
+      "Content-Type": "application/json",
+      "X-Goog-Api-Key": API_KEY,
+      "X-Goog-FieldMask":
+        "routes.duration,routes.distanceMeters,routes.polyline,routes.legs.polyline,routes.legs.steps.polyline",
+    };
+
+    const data = {
+      origin: {
+        address: origin,
+      },
+      destination: {
+        address: destination,
+      },
+      travelMode: "DRIVE",
+    };
+
+    const response = await axios.post(
+      "https://routes.googleapis.com/directions/v2:computeRoutes",
+      data,
+      { headers }
+    );
+
+    return response;
+  } catch (error) {
+    console.error(error); // handle the error here
+  }
 };
-
-const encoded =
-  "skzvHieyrLuIoBek@eO_c@sK{Ae@sLwCqC_@sO{AqEi@eEa@}Fs@iMeAuHy@cTkBw@OaOaBMAyFdq@B|@o@|H{@c@sDgAAM";
-const decoded = decode(encoded);
-
-const onLoad = (polyline) => {
-  console.log("polyline: ", polyline);
-};
-
-const path = [];
-
-const options = {
-  strokeColor: "#FF0000",
-  strokeOpacity: 0.8,
-  strokeWeight: 2,
-  fillColor: "#FF0000",
-  fillOpacity: 0.35,
-  clickable: false,
-  draggable: false,
-  editable: false,
-  visible: true,
-  radius: 30000,
-  zIndex: 1,
-};
-
-let sumLat = 0;
-let sumLng = 0;
-
-for (let i = 0; i < decoded.length; i++) {
-  console.log(decoded[i][0], decoded[i][1]);
-  path.push({ lat: decoded[i][0], lng: decoded[i][1] });
-  sumLat += decoded[i][0];
-  sumLng += decoded[i][1];
-}
-
-center.lat = sumLat / decoded.length;
-center.lng = sumLng / decoded.length;
-
-let zoom = 13;
 
 const MyComponent = () => {
   const [width, height] = useWindowSize();
 
+  const [response, setResponse] = useState({});
+
+  let origin = "г. Астана, Кабанбай батыр 53";
+  let destination = "г. Астана, Хан шатыр";
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await computeRoutes(origin, destination);
+      setResponse(response);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log("response: ");
+  console.log(response);
+
+  const center = {
+    lat: 0,
+    lng: -180,
+  };
+
+  const encoded = response.data?.routes[0]?.polyline?.encodedPolyline || "";
+  const expTime = response.data?.routes[0]?.duration || "";
+  const decoded = decode(encoded);
+
+  const onLoad = (polyline) => {
+    console.log("polyline: ", polyline);
+  };
+
+  const path = [];
+
+  const options = {
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    visible: true,
+    radius: 30000,
+    zIndex: 1,
+  };
+
+  let sumLat = 0;
+  let sumLng = 0;
+
+  for (let i = 0; i < decoded.length; i++) {
+    console.log(decoded[i][0], decoded[i][1]);
+    path.push({ lat: decoded[i][0], lng: decoded[i][1] });
+    sumLat += decoded[i][0];
+    sumLng += decoded[i][1];
+  }
+
+  center.lat = sumLat / decoded.length;
+  center.lng = sumLng / decoded.length;
+
+  let zoom = 13;
+
   const mapContainerStyle = {
     height: height + "px",
-    width: width / 2 + "px",
+    width: width > 700 ? width / 2 + "px" : width + "px",
   };
   return (
-    <div
-      className={styles.container}
-      style={{ width: "100%", height: "100%", marginLeft: "25%" }}
-    >
-      <LoadScript googleMapsApiKey="AIzaSyD-iUnnCzlz-eGSWYRJnfEvSA_lhg24CqU">
-        <GoogleMap
-          id="marker-example"
-          mapContainerStyle={mapContainerStyle}
-          zoom={zoom}
-          center={center}
-        >
-          <Polyline
-            onLoad={onLoad}
-            geodesic={true}
-            path={path}
-            options={options}
-          />
-        </GoogleMap>
-      </LoadScript>
+    <div className={styles.devider}>
+      <div className={styles.leftInfo}>
+        <div className={styles.mapInfo}>
+          <div className={styles.wrap}>
+            <div className={styles.docInfo}>
+              <p>Документы в Пути!</p>
+            </div>
+            <div className={styles.docsTime}>
+              <p>Примерное время ожидания</p>
+              <p>{(+expTime.slice(0, -1) / 60).toFixed() + " мин"}</p>
+            </div>
+            <div className={styles.docAnimate}>
+              <img className={styles.curAnime} src={Running} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <LoadScript googleMapsApiKey="AIzaSyD-iUnnCzlz-eGSWYRJnfEvSA_lhg24CqU">
+          <GoogleMap
+            id="marker-example"
+            mapContainerStyle={mapContainerStyle}
+            zoom={zoom}
+            center={center}
+          >
+            <Polyline
+              onLoad={onLoad}
+              geodesic={true}
+              path={path}
+              options={options}
+            />
+          </GoogleMap>
+        </LoadScript>
+      </div>
     </div>
   );
 };
 
-export default React.memo(MyComponent);
+export default MyComponent;
