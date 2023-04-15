d318a78bef8b69ff311d517cdc2132e8dbacf023
Olzhas <olzhas@Olzhass-MacBook-Air.local>
2023-04-15 12:16:00 +0600

    0.01

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Courier from "./pages/Courier/Courier";
+import Home from "./pages/Home/Home";
+import Payment from "./pages/Payment/Payment";
+import SMSAuth from "./pages/SMSAuth/SMSAuth";
+
 const App = () => {
   return (
-    <div className="App">
-      
-    </div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/Courier" element={<Courier />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Payment" element={<Payment />} />
+        <Route path="/SMSAuth" element={<SMSAuth />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/SMSAuth/SMSAuth.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SMSAuth = () => {
+  return (
+    <div>SMSAuth</div>
+  )
+}
+
+export default SMSAuth;
